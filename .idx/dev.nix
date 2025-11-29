cde0dcda7ab4da72fdbf2767eeb733b1d901e46e
Butterdime <butterdime@gmail.com>
2025-11-29 21:05:22 +0800

    Fix Nix dev environment: use system OpenCV

---
+++ .idx/dev.nix
@@ -0,0 +1,12 @@
+{ pkgs }:
+
+pkgs.mkShell {
+  packages = [
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.opencv
+    pkgs.python311Packages.pytesseract
+    pkgs.python311Packages.pillow
+    pkgs.python311Packages.numpy
+  ];
+}
